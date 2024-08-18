091a1dd670f5570f06031c5cd7d7f1afe9787277
michaeljumah <michaeljumah8@gmail.com>
2024-08-18 23:39:47 +0300

    re

--- RomanToNumber.py
+++ RomanToNumber.py
@@ -13,7 +13,7 @@ L             50
 C             100
 D             500
 M             1000
-For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
 
 Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
 
@@ -68,7 +68,8 @@ class Solution(object):
                 # Subtract the current value if the next value is greater
                 total -= roman[s[i]]
             else:
-                # Add the current value otherwise
+                
+                #Add the current value otherwise
                 total += roman[s[i]]
                 
                 
